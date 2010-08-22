9e92b00b0ede96b7ae6b5cddb1bf49b0e348fadc
Phil Cooper-King <phil@cooperking.net>
2010-08-22 21:14:53 +0100

    Added some screenshots, updated Readme, and added a fading blood effect. Goodnight!

--- LD18_Source/src/blood.hpp
+++ LD18_Source/src/blood.hpp
@@ -10,6 +10,8 @@ class Blood : public GameObject
 
 	int type;
 	unsigned long lifetime;
+	Gosu::Color bloodAlpha;
+	int alpha;
 
 public:
 	Blood(int setx, int sety) : GameObject()
@@ -22,23 +24,30 @@ public:
 		type = (rand() % 2) + 1;
 
 		lifetime = Gosu::milliseconds() + 10000;
+		alpha = 255;
+		bloodAlpha.setAlpha(alpha);
+		bloodAlpha.setBlue(255);
+		bloodAlpha.setRed(255);
+		bloodAlpha.setGreen(255);
 	}
 
 	bool update()
 	{
-		return (lifetime > Gosu::milliseconds() ? true : false);
+		bloodAlpha.setAlpha(alpha -= 1);
+		//return (lifetime > Gosu::milliseconds() ? true : false);
+		return (alpha ? true : false);
 	}
 
 	void draw()
 	{
 		if (type == 1) {
 			static Gosu::Image blood1(windowInstance().graphics(), Directory::MediaDir + L"blood1.png", false);
-			blood1.draw(x, y, Layer::BLOOD_SPLATTER_LAYER, 1, 1, 0x99ffffff);
+			blood1.draw(x, y, Layer::BLOOD_SPLATTER_LAYER, 1, 1, bloodAlpha);
 		}
 
 		if (type == 2) {
 			static Gosu::Image blood2(windowInstance().graphics(), Directory::MediaDir + L"blood2.png", false);
-			blood2.draw(x, y, Layer::BLOOD_SPLATTER_LAYER, 1, 1, 0x99ffffff);
+			blood2.draw(x, y, Layer::BLOOD_SPLATTER_LAYER, 1, 1, bloodAlpha);
 		}
 
 	}
--- README.txt
+++ README.txt
@@ -5,8 +5,16 @@ LD18 Entry for Phil CK (Basic on LD)
 
 All code is free to look, poke, and prod, but I must warn you; Put some boots on as its a mess :(
 
+I was trying to improve the sounds in the game, but I ran out of fuel :(
 
-Thanks To:
+Requirements
+============
+OSX 10.5+ - I did run into some problems with 10.5
+Win XP+   - Untested on XP, but it should work
+
+
+Thanks To
+=========
 Jlnr and Gosu IRC for Gosu help.
 DrPetter and J.Bengtsson SFXR/CFXR for the sounds.
 Roisin for being patient with me.
@@ -15,5 +23,6 @@ Meagan for snacks and drinks :)
 
 Known Bugs
 ==========
-Mac - Sound Volume sometimes spikes
-Mac - Sometimes the player will run off to one side and get stuck
+Mac/Win - A little slow down at the beginning of the game.
+Mac - Sound Volume sometimes spikes.
+Mac - Sometimes the player will run off to one side and get stuck.
